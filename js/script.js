2d412134f3109a2b8b7ee4a6649592eae4028a0e
JoepH24 <joep.honnebier@hva.nl>
2025-06-01 21:42:56 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ let vermoeidheid = 0;
 let honger = 0;
 
 // Elementen in de HTML ophalen via hun ID, zodat we ze kunnen updaten of gebruiken
-// → getElementById geleerd van de lessen Slides week 1
+// getElementById geleerd van de lessen Slides week 1
 const motivatieEl = document.getElementById("motivatie");
 const energieEl = document.getElementById("energie");
 const hongerEl = document.getElementById("honger");
 const bartjeImg = document.getElementById("bartje");
 
 // Alle <button>-elementen ophalen (er worden meerdere knoppen gebruikt)
-// → querySelectorAll van W3Schools: https://www.w3schools.com/jsref/met_document_queryselectorall.asp
+// querySelectorAll van W3Schools: https://www.w3schools.com/jsref/met_document_queryselectorall.asp
 const knoppen = document.querySelectorAll("button");
 x
 
@@ -21,7 +21,7 @@ const etenAudio = document.getElementById("etenAudio");
 const pauzeAudio = document.getElementById("pauzeAudio");
 
 // Functie om muziek af te spelen of te pauzeren
-// → Muziek toggle logica gebaseerd op W3Schools audio voorbeelden: https://www.w3schools.com/tags/av_prop_paused.asp
+// Muziek toggle logica gebaseerd op W3Schools audio voorbeelden: https://www.w3schools.com/tags/av_prop_paused.asp
 function toggleMusic() {
   const music = document.getElementById("backgroundMusic");
   if (music.paused) {
@@ -49,15 +49,15 @@ function toonTijdelijkeAfbeelding(actieSrc, geluid) {
 // hierbij begon ik met hulp van de docent waarna ik met mijn eigen onderzoek via W3schools heb uitgevogeld hoe ik de audio toevoegde
 knoppen[0].onclick = () => {
   if (hopeloosheid > 0) hopeloosheid--;
-  toonTijdelijkeAfbeelding("/images/7koffie.jpg", koffieAudio);
+  toonTijdelijkeAfbeelding("images/7koffie.jpg", koffieAudio);
 };
 knoppen[1].onclick = () => {
   if (vermoeidheid > 0) vermoeidheid--;
-  toonTijdelijkeAfbeelding("/images/5valtinslaap.jpg", pauzeAudio);
+  toonTijdelijkeAfbeelding("images/5valtinslaap.jpg", pauzeAudio);
 };
 knoppen[2].onclick = () => {
   if (honger > 0) honger--;
-  toonTijdelijkeAfbeelding("/images/6eten.jpg", etenAudio);
+  toonTijdelijkeAfbeelding("images/6eten.jpg", etenAudio);
 };
 
 
@@ -82,15 +82,16 @@ function render() {
 
   // Afbeelding wisselt afhankelijk van afhankelijk van Bartje z'n toestand, De "If Else" leerde we in de slides van Les 1b
   if (totaal <= 15) {
-    bartjeImg.src = "/images/2vrolijk.jpg";
+    bartjeImg.src = "images/2vrolijk.jpg";
   } else if (totaal <= 30) {
-    bartjeImg.src = "/images/1neutraal.jpg";
+    bartjeImg.src = "images/1neutraal.jpg";
   } else if (totaal <= 45) {
-    bartjeImg.src = "/images/4booshongerig.jpg";
+    bartjeImg.src = "images/4booshongerig.jpg";
   } else if (totaal <= 60) {
-    bartjeImg.src = "/images/3chagrijnig.jpg";
+    bartjeImg.src = "images/3chagrijnig.jpg";
   } else {
-    bartjeImg.src = "/images/8kapot.jpg";
+    bartjeImg.src = "images/8kapot.jpg";
   }
 }
 
+// Mijn afbeeldingen zijn allemaal gegenereerd met Dreamlab, de audio van de knoppen heb ik zelf opgenomen, de muziek heb ik gegenereerd met Udio (dit hebben we geleerd bij Emerging Technologies)
